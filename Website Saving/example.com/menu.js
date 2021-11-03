2e4f7b19542b231640313927edd9c629cb4cc4e6
Megalaoofania <92765223+Megalaoofania@users.noreply.github.com>
2021-11-02 21:41:40 -0500

    Add files via upload

---
+++ Website Saving/example.com/menu.js
@@ -0,0 +1,16 @@
+function view_index_html()
+{
+    location.href = '/Code/examplehtml.txt'
+}
+function view_bubble_compiled_js()
+{
+    location.href = '/Code/examplebubblecompiledjs.txt'
+}
+function view_pretty_print_bubble_compiled_js()
+{
+    location.href = '/Code/exampleprettyprintbubblecompiledjs.txt'
+}
+function view_website()
+{
+    location.href = '/example.com/index.html'
+}
