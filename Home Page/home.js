b4d110737a82815a3dd875840de3656adcb0d3e9
Megalaoofania <92765223+Megalaoofania@users.noreply.github.com>
2021-11-03 19:40:34 -0500

    Add files via upload

--- Home Page/home.js
+++ Home Page/home.js
@@ -4,12 +4,12 @@ console.log("Received.")
 if(document.getElementById("searchweb").value == "example.com")
 {
     console.log("Loading...")
-    location.href = 'Ultra-Code-Saver/example.com/menu.html'
+    location.href = '/example.com/menu.html'
 } else if(document.getElementById("searchweb").value == "testsite1.com") {
     console.log("Loading...")
-    location.href = 'Ultra-Code-Saver/Test%20Site/testsite1.html'
+    location.href = '/Test%20Site/testsite1.html'
 } else {
     console.log("Loading...")
-    location.href = "Ultra-Code-Saver/Error/Error404.html"
+    location.href = "/Error/Error404.html"
 }
 }
